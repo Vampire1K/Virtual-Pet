405fd5d6457e81ff57122684a1c2469437cd2bad
Katherine Pierce <77492568+Vampire1K@users.noreply.github.com>
2021-04-12 18:53:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//Create variables here
+var dog, happyDog;
+var database
+var foodS, foodStock;
+
+
+function preload()
+{
+	//load images here
+
+
+  
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
